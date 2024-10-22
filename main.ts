da1bb1df2fc1c9d2d07db5ab060f2583b3987d64
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2024-10-22 11:12:58 -0400

    made the code

--- main.ts
+++ main.ts
@@ -2,7 +2,41 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program generates random numbers and compares 
 */
 
-basic.showString('Hello, World!')
+let randomNumber1: number 
+let randomNumber2: number
+
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+//#1 number 
+input.onButtonPressed(Button.A, function () {
+    randomNumber1 = randint(0,99)
+    basic.clearScreen()
+    basic.showString('# 1 :' + randomNumber1)
+    basic.showIcon(IconNames.Happy)
+})
+
+//#2 number 
+input.onButtonPressed(Button.B, function () {
+    randomNumber2 = randint(0, 99)
+    basic.clearScreen()
+    basic.showString('# 2 :' + randomNumber2)
+    basic.showIcon(IconNames.Happy)
+})
+
+//shake 
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    if (randomNumber1 > randomNumber2){
+        basic.showString(randomNumber1 + '>' + randomNumber2)
+    }
+    else{
+        randomNumber1 < randomNumber2
+        basic.showString(randomNumber1 + '<' + randomNumber2)
+    }
+    basic.showIcon(IconNames.Happy)
+})
